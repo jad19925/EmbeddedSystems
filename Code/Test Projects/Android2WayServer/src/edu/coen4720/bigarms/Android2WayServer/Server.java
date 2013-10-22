421d6f34748eb4869a0d12386019e4e7b2ca6ac8
Jacob Dahleen <jacob.dahleen@gmail.com>
2013-10-22 11:48:00 -0500

    finish initial merge of gps and network code

--- Code/Test Projects/Android2WayServer/src/edu/coen4720/bigarms/Android2WayServer/Server.java
+++ Code/Test Projects/Android2WayServer/src/edu/coen4720/bigarms/Android2WayServer/Server.java
@@ -14,43 +14,43 @@ package edu.coen4720.bigarms.Android2WayServer;
 
 import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
-import java.net.NetworkInterface;
-import java.net.ServerSocket;
-import java.net.Socket;
 import java.net.SocketException;
 import java.net.UnknownHostException;
-import java.util.Enumeration;
 
 import edu.coen4720.bigarms.Android2WayServer.R;
 
 import android.app.Activity;
+import android.content.Context;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.net.wifi.WifiInfo;
 import android.net.wifi.WifiManager;
 import android.os.Bundle;
 import android.os.Handler;
 import android.widget.TextView;
+import android.widget.Toast;
 
-public class Server extends Activity {
-	
+public class Server extends Activity implements LocationListener {
+	//network class values
 	private DatagramSocket outSocket;
-
 	Handler updateConversationHandler;
-
 	Thread serverThread = null;
-
 	private TextView text;
-
 	public static final int SERVERPORT = 8080;
-	
 	public static final int VEXPORT = 9923;
 	private static final String VEX_IP = "192.168.1.127";
-	
-	private static final boolean UDP = true;
 
+	//gps class values
+	private TextView latituteField;
+	private TextView longitudeField;
+	private LocationManager locationManager;
+	private String provider;
+	
 	public String getIpAddr() {
 		//Get Wi-Fi information
 	    WifiManager wifiManager = (WifiManager) getSystemService(WIFI_SERVICE);
@@ -70,18 +70,36 @@ public class Server extends Activity {
 	 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
-
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
 		
 		//Text Field
 		text = (TextView) findViewById(R.id.text2);
+		latituteField = (TextView) findViewById(R.id.TextView02);
+	    longitudeField = (TextView) findViewById(R.id.TextView04);
 		
 		//Handler that receives information from Client
 		updateConversationHandler = new Handler();
 
 		new Thread(new CommunicationThread()).start();
 		new Thread(new ClientThread()).start();
+		
+		//gps code
+		// Get the location manager
+	    locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+	    // Define the criteria how to select the location provider -> use default
+	    Criteria criteria = new Criteria();
+	    provider = locationManager.getBestProvider(criteria, false);
+	    Location location = locationManager.getLastKnownLocation(provider);
+
+	    // Initialize the location fields
+	    if (location != null) {
+	      System.out.println("Provider " + provider + " has been selected.");
+	      onLocationChanged(location);
+	    } else {
+	      latituteField.setText("Location not available");
+	      longitudeField.setText("Location not available");
+	    }
 	}
 
 	class CommunicationThread implements Runnable {
@@ -175,4 +193,46 @@ public class Server extends Activity {
 			}
 		}
 	}
+	
+	/* Request updates at startup */
+	  @Override
+	  protected void onResume() {
+	    super.onResume();
+	    locationManager.requestLocationUpdates(provider, 1, 1, this);
+	    
+	  }
+
+	  /* Remove the locationlistener updates when Activity is paused */
+	  @Override
+	  protected void onPause() {
+	    super.onPause();
+	    locationManager.removeUpdates(this);
+	  }
+
+	  @Override
+	  public void onLocationChanged(Location location) {
+	    double lat = (location.getLatitude());
+	    double lng = (location.getLongitude());
+	    latituteField.setText(String.valueOf(lat));
+	    longitudeField.setText(String.valueOf(lng));
+	  }
+
+	  @Override
+	  public void onStatusChanged(String provider, int status, Bundle extras) {
+
+
+	  }
+
+	  @Override
+	  public void onProviderEnabled(String provider) {
+	    Toast.makeText(this, "Enabled new provider " + provider,
+	        Toast.LENGTH_SHORT).show();
+
+	  }
+
+	  @Override
+	  public void onProviderDisabled(String provider) {
+	    Toast.makeText(this, "Disabled provider " + provider,
+	        Toast.LENGTH_SHORT).show();
+	  }
 }
