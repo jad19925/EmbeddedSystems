711192e7779bef1b4360a85ab520538ce64aea6f
Jacob Dahleen <jacob.dahleen@gmail.com>
2013-10-29 15:19:41 -0500

    Updating the two-way server to fix moving to waypoints and add feature to send location back to the tablet

--- Code/Test Projects/Android2WayServer/src/edu/coen4720/bigarms/Android2WayServer/Server.java
+++ Code/Test Projects/Android2WayServer/src/edu/coen4720/bigarms/Android2WayServer/Server.java
@@ -43,13 +43,16 @@ import android.widget.Toast;
 public class Server extends Activity implements LocationListener, SensorEventListener {
 	//network class values
 	private DatagramSocket outSocket;
+	private DatagramSocket tabletSocket;
 	Handler updateConversationHandler;
 	Thread serverThread = null;
 	private TextView text;
 	public static final int SERVERPORT = 8080;
 	public static final int VEXPORT = 9923;
+	public static final int TABLETPORT = 7777;
 	private static final String VEX_IP = "192.168.1.127";
-
+	private static final String TABLET_IP = "192.168.1.132";
+	
 	//gps class values
 	private TextView latitudeField;
 	private TextView longitudeField;
@@ -59,7 +62,7 @@ public class Server extends Activity implements LocationListener, SensorEventLis
 	private ScrollView messageScroll;
 	private LocationManager locationManager;
 	private String provider;
-	private final String networkProvider = locationManager.NETWORK_PROVIDER;
+	private final String networkProvider = LocationManager.NETWORK_PROVIDER;
 	private SensorManager mSensorManager;
 	private float[] accValues = null;
 	private float[] magValues = null;
@@ -71,7 +74,7 @@ public class Server extends Activity implements LocationListener, SensorEventLis
 	private double destLon = -87.929579;
 	private double destBearing = 0;
 	private double destDistance = 0;
-	//private String lastCommand;
+	private String lastCommand = "null";
 	private boolean autoMove = false;
 	private boolean manMove = false;
 	
@@ -110,6 +113,7 @@ public class Server extends Activity implements LocationListener, SensorEventLis
 
 		new Thread(new CommunicationThread()).start();
 		new Thread(new ClientThread()).start();
+		new Thread(new TabletThread()).start();
 		
 		//gps code
 		// Get the location manager
@@ -117,7 +121,6 @@ public class Server extends Activity implements LocationListener, SensorEventLis
 	    // Define the criteria how to select the location provider -> use default
 	    Criteria criteria = new Criteria();
 	    provider = locationManager.getBestProvider(criteria, false);
-	    boolean test = locationManager.isProviderEnabled(networkProvider);
 	    Location location = locationManager.getLastKnownLocation(provider);
 	    
 	    // TextView that will tell the user what degree is he heading
@@ -184,9 +187,23 @@ public class Server extends Activity implements LocationListener, SensorEventLis
 			} catch (IOException e1) {
 				e1.printStackTrace();
 			}
-
 		}
+	}
+	
+	class TabletThread implements Runnable {
+		@Override
+		public void run() {
 
+			try {
+				tabletSocket = new DatagramSocket();
+				tabletSocket.connect(InetAddress.getByName(TABLET_IP), TABLETPORT);
+
+			} catch (UnknownHostException e1) {
+				e1.printStackTrace();
+			} catch (IOException e1) {
+				e1.printStackTrace();
+			}
+		}
 	}
 
 	class ProcessMessageThread implements Runnable {
@@ -280,57 +297,18 @@ public class Server extends Activity implements LocationListener, SensorEventLis
 		toDestBField.setText(String.valueOf(bearing[0]));
 		toDestDField.setText(String.valueOf(bearing[1]));
 		
-		//send control messages
-//		boolean send = false;
-//		String msg;
-//		byte[] data = new byte[1024];
-//		DatagramPacket dPack = new DatagramPacket(data,1024);
-//		
-//		if(manMove) {
-//			//do nothing, gui is in control
-//			System.out.println("manual movement");
-//			text.setText(text.getText().toString() + "manual movement\n");
-//		}
-//		else if (autoMove) {
-//			//movement algorithm for moving to waypoint
-//			System.out.println("doing automove");
-//			text.setText(text.getText().toString() + "doing automove\n");
-//			while (facing !=  bearing[0]){
-//				if(facing - destBearing < -3){
-//					//set msg to turn left
-//					msg = "left";
-//					dPack.setData(msg.getBytes());
-//					send = true;
-//					
-//				}
-//				else if (destBearing - facing < -3){
-//					//set msg turn right
-//					msg = "right";
-//					dPack.setData(msg.getBytes());
-//					send = true;
-//				}
-//				else {
-//					msg = "forward";
-//					dPack.setData(msg.getBytes());
-//					send = true;
-//				}
-//			}
-//		}
-//		else {
-//			//send stop messages
-//			dPack.setData(new String("stop").getBytes());
-//			send = true;
-//			//System.out.println("stopping");
-//		}
-//		
-//		if(send){
-//			try {
-//				outSocket.send(dPack);
-//			} catch (IOException e) {
-//				// TODO Auto-generated catch block
-//				e.printStackTrace();
-//			}
-//		}
+		//send location back to the tablet
+		String msg;
+		byte[] data = new byte[1024];
+		DatagramPacket dPack = new DatagramPacket(data,1024);
+		msg = "lat" + String.valueOf(lat) + "lon" + String.valueOf(lng);
+		dPack.setData(msg.getBytes());
+		try {
+			tabletSocket.send(dPack);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 	@Override
@@ -342,7 +320,6 @@ public class Server extends Activity implements LocationListener, SensorEventLis
 	public void onProviderEnabled(String provider) {
 	    Toast.makeText(this, "Enabled new provider " + provider,
 	        Toast.LENGTH_SHORT).show();
-
 	}
 
 	@Override
@@ -382,44 +359,58 @@ public class Server extends Activity implements LocationListener, SensorEventLis
 		
 		if(manMove) {
 			//do nothing, gui is in control
-			System.out.println("manual movement");
-			//text.setText(text.getText().toString() + "manual movement\n");
+			//System.out.println("manual movement");
 		}
 		else if (autoMove) {
 			//movement algorithm for moving to waypoint
-			System.out.println("doing automove");
-			//text.setText(text.getText().toString() + "doing automove\n");
+			//System.out.println("doing automove");
 			if(destDistance > 3){
-				if(facing - destBearing < -3){
+				if(facing - destBearing > 3){
 					//set msg to turn left
 					msg = "left";
 					dPack.setData(msg.getBytes());
-					send = true;
+					send = !lastCommand.equals("left");
+					lastCommand = "left";
 				}
-				else if (destBearing - facing < -3){
+				else if (destBearing - facing > 3){
 					//set msg turn right
 					msg = "right";
 					dPack.setData(msg.getBytes());
-					send = true;
+					send = !lastCommand.equals("right");
+					lastCommand = "right";
 				}
 				else {
-					msg = "forward";
+					if(lastCommand.equals("left") || lastCommand.equals("right")) {
+						msg = "stop";
+						lastCommand = "stop";
+						send = true;
+					}
+					else if(lastCommand.equals("stop")) {
+						msg = "forward";
+						lastCommand = "forward";
+						send = true;
+					}
+					else{
+						msg = "stop";
+						send = false;
+					}
 					dPack.setData(msg.getBytes());
-					send = true;
 				}
 			}
 			else {
 				//stop when we reach the destination
 				msg = "stop";
 				dPack.setData(msg.getBytes());
-				send = true;
+				send = !lastCommand.equals("stop");
+				lastCommand = "stop";
 			}
 		}
 		else {
 			//send stop messages
 			msg = "stop";
 			dPack.setData(msg.getBytes());
-			send = true;
+			send = !lastCommand.equals("stop");
+			lastCommand = "stop";
 		}
 		
 		if(send){
