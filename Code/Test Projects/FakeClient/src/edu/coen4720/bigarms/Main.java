e2e0d8a142327367640b1251129aeac7a38ec774
Jacob Dahleen <jacob.dahleen@gmail.com>
2013-12-03 10:52:02 -0600

    Update fake client

--- Code/Test Projects/FakeClient/src/edu/coen4720/bigarms/Main.java
+++ Code/Test Projects/FakeClient/src/edu/coen4720/bigarms/Main.java
@@ -5,30 +5,34 @@ import java.util.Scanner;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		FakeClient fc = new FakeClient();
 		Scanner scanner = new Scanner(System.in);
 		System.out.println("enter commands:");
 		int currentSpeed = 15;
 		boolean exit = false;
+		int divider = 22; //inside = 41
+		int offset = 1750; //inside = 1700
 		
 		while(!exit) {
 			String inString = scanner.nextLine();
+			int sleep;
 			
 			switch(inString) {
 				case "forward":
-					//fc.sendMessage("MoveR100L100");
-					fc.sendMessage("forward");
+					fc.sendMessage("moveR100L100");
+					//fc.sendMessage("forward");
 					break;
 				case "backward":
-					//fc.sendMessage("MoveR-100L-100");
-					fc.sendMessage("backward");
+					fc.sendMessage("moveR-100L-100");
+					//fc.sendMessage("backward");
 					break;
 				case "left":
-					fc.sendMessage("left");
+					fc.sendMessage("moveR100L-100");
+					//fc.sendMessage("left");
 					break;
 				case "right":
-					fc.sendMessage("right");
+					fc.sendMessage("moveR-100L100");
+					//fc.sendMessage("right");
 					break;
 				case "stop":
 					fc.sendMessage("stop");
@@ -55,10 +59,33 @@ public class Main {
 					exit = true;
 					fc.sendMessage("exit");
 					break;
+				case "r90":
+					sleep = offset + (1000*90)/divider;
+					System.out.println("sleep = " + sleep);
+					fc.sendMessage("moveR-100L100");
+					try {
+						Thread.sleep(sleep);
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}
+					fc.sendMessage("stop");
+					break;
+				case "r360":
+					sleep = offset + (1000*360)/divider;
+					System.out.println("sleep = " + sleep);
+					fc.sendMessage("moveR-100L100");
+					try {
+						Thread.sleep(sleep);
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}
+					fc.sendMessage("stop");
+					break;
 				default:
 					fc.sendMessage(inString);
 					break;
 			}
 		}
+		scanner.close();
 	}
 }
