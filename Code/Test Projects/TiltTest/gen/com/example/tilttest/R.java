53ed99d5e9fe7cccc37019476a44551a1081ed66
Jacob Dahleen <jacob.dahleen@gmail.com>
2013-12-03 10:53:15 -0600

    Update tilt test

--- Code/Test Projects/TiltTest/gen/com/example/tilttest/R.java
+++ Code/Test Projects/TiltTest/gen/com/example/tilttest/R.java
@@ -26,11 +26,10 @@ or to a theme attribute in the form "<code>?[<i>package</i>:][<i>type</i>:]<i>na
     }
     public static final class id {
         public static final int LinearLayout1=0x7f070001;
-        public static final int dummy_button=0x7f070003;
         public static final int fullscreen_content=0x7f070000;
-        public static final int fullscreen_content_controls=0x7f070002;
-        public static final int textView1=0x7f070005;
-        public static final int textView2=0x7f070004;
+        public static final int textView1=0x7f070004;
+        public static final int textView2=0x7f070003;
+        public static final int textView3=0x7f070002;
     }
     public static final class layout {
         public static final int activity_fullscreen=0x7f030000;
